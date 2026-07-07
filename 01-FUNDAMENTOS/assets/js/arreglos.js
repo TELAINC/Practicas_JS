21a76656ff1558e632449ab17a8eeed7c5552cd5
carlos <carlos.edu.96558061@gmail.com>
2026-07-06 18:48:41 -0600

    se aprende sobre arreglos

---
+++ 01-FUNDAMENTOS/assets/js/arreglos.js
@@ -0,0 +1,26 @@
+//const arr = new Array(10);
+//const arr = [];
+//console.log(arr);
+
+let videoJuegos = ['Halo', 'COD', 'DMC5'];
+console.log({ videoJuegos });
+console.log(videoJuegos);
+console.log(videoJuegos[0]);
+
+let arregloYi = [
+    true,
+    123,
+    'Carlos',
+    1 + 2,
+    function () {},
+    () => {},
+    { a: 1 },
+    ['Jefe', 'Halo', 'Inquisidor', 'Covenant', [
+        'Kelly',
+        'Cortana',
+    ]]
+];
+
+//console.log({ arregloYi });
+//console.log(arregloYi[2]);
+console.log(arregloYi[7][4][1]);
